1855f859730d71d330da1d4bcac845140077720b
pichead <61868523+pichead@users.noreply.github.com>
2024-11-23 20:10:00 +0700

    change port to 3333

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
     allowedHeaders: '*',
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE']
   });
-  await app.listen(env.appPort);
+  await app.listen(3333);
   console.log("server is running on : http://localhost:" + env.appPort)
   initializeSwagger(app)
 
